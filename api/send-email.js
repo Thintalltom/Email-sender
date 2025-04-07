c39ad1d9a2a0aca61079324d690c267432655733
Thintalltom <adeyanjutomide@gmail.com>
2025-04-07 15:11:35 +0100

    deleted vercel.json file

--- api/send-email.js
+++ api/send-email.js
@@ -1,23 +1,7 @@
 // /api/send-email.js
 
 import nodemailer from 'nodemailer';
-
-const allowCors = fn => async (req, res) => {
-  res.setHeader('Access-Control-Allow-Credentials', true);
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-  );
-
-  if (req.method === 'OPTIONS') {
-    res.status(200).end();
-    return;
-  }
-
-  return await fn(req, res);
-};
+import allowCors from './allowCors';
 
 const handler = async (req, res) => {
   if (req.method === 'POST') {
